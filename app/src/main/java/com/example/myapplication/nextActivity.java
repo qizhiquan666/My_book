74a96d1bc3e294d6eea29b152cb5ce960f8e9aff
qizhiquan666 <421763988@qq.com>
2020-04-22 16:02:17 +0800

    增加上一章和下一章逻辑

--- app/src/main/java/com/example/myapplication/nextActivity.java
+++ app/src/main/java/com/example/myapplication/nextActivity.java
@@ -2,8 +2,7 @@ package com.example.myapplication;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.AlertDialog;
-import android.content.DialogInterface;
-import android.net.nsd.NsdManager;
+import android.content.Context;
 import android.util.Log;
 import android.app.Dialog;
 import android.content.Intent;
@@ -15,39 +14,52 @@ import android.view.Display;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+import java.io.IOException;
+import java.util.ArrayList;
 
-public class nextActivity  extends Activity {
-    int b;
+public class nextActivity extends Activity {
+    int b, nunber1;
     private Dialog progressDialog;
+    String chapter_url = "";
+    private TextView msg;
+
     @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_main);
         Intent i = getIntent();
-        String string1 = i.getStringExtra("string");
+        String string1 = i.getStringExtra("content");
+        String string2 = i.getStringExtra("title");
+        final String nunber = i.getStringExtra("nunber");
+        final String paixu = i.getStringExtra("paixu");
+        nunber1 = Integer.valueOf(nunber).intValue();
+        final ArrayList<String> list_chapter_url = (ArrayList<String>) getIntent().getStringArrayListExtra("list_chapter_url");
         final TextView my_string = (TextView) findViewById(R.id.tx6);
+        final TextView title = (TextView) findViewById(R.id.title1);
+        title.setText(string2);
         my_string.setText("        " + string1);
         my_string.scrollTo(0, 0);
         my_string.setMovementMethod(ScrollingMovementMethod.getInstance());
         b = 0;
-        progressDialog = new Dialog(nextActivity.this, R.style.DialogTheme);
-        progressDialog.setContentView(R.layout.set);
+        progressDialog = new Dialog(nextActivity.this, R.style.progress_dialog);
+        progressDialog.setContentView(R.layout.dialog);
         progressDialog.setCancelable(true);
         progressDialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
-        WindowManager windowManager = getWindowManager();
-        Display display = windowManager.getDefaultDisplay();
-        WindowManager.LayoutParams lp = progressDialog.getWindow().getAttributes();
-        lp.width = (int) (display.getWidth()); //设置宽度
-        progressDialog.getWindow().setAttributes(lp);
-        progressDialog.getWindow().setGravity(Gravity.BOTTOM);
+        msg = (TextView) progressDialog.findViewById(R.id.id_tv_loadingmsg);
+
         my_string.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(final View v) {
                 int a = my_string.getScrollY();
                 if (a - b > 10) {
                     b = a;
@@ -59,17 +71,142 @@ public class nextActivity  extends Activity {
                     final EditText edit2 = (EditText) layout.findViewById(R.id.edit2);
                     final Dialog aler = new AlertDialog.Builder(nextActivity.this)
                             .setView(layout)
-                            .setPositiveButton("修改", new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                    String a = edit2.getText().toString();
-                                    float c = Float.parseFloat(a);
-                                    my_string.setTextSize(c);
-                                    edit2.setText(edit2.getText().toString());
+                            .show();
+                    Button button = (Button) layout.findViewById(R.id.set_size);
+                    button.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            String d = edit2.getText().toString();
+                            float c = Float.parseFloat(d);
+                            my_string.setTextSize(c);
+                            title.setTextSize(c);
+                            edit2.setText(edit2.getText().toString());
+                        }
+                    });
+                    TextView up = (TextView) layout.findViewById(R.id.up);
+                    final TextView down = (TextView) layout.findViewById(R.id.down);
+                    up.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            msg.setText("搜索中,请稍等");
+                            progressDialog.show();
+                            try {
+                                if (paixu.equals("降序")) {
+                                    nunber1 = nunber1 + 1;
+                                } else {
+                                    nunber1 = nunber1 - 1;
                                 }
-                            }).show();
+                                if (nunber1 < 0) {
+                                    progressDialog.dismiss();
+                                    nunber1 = nunber1 + 1;
+                                    Toast.makeText(nextActivity.this, "没有上一章了", Toast.LENGTH_LONG).show();
+                                    return;
+                                }
+                                chapter_url = list_chapter_url.get(nunber1);
+                                if (!chapter_url.equals("")) {
+                                    new Thread(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            try {
+                                                System.out.print(chapter_url);
+                                                Document doc = Jsoup.connect(chapter_url).get();
+                                                Elements btEl = doc.select("#content");
+                                                final String data_title = doc.select("h1").text();
+                                                btEl.select("br").next().append("\\n");
+                                                String bt1 = btEl.text();
+                                                final String data_text = bt1.replace("\\n", "\n       ");
+                                                if (!data_text.equals("")) {
+                                                    progressDialog.dismiss();
+                                                    Handler mainHandler = new Handler(Looper.getMainLooper());
+                                                    mainHandler.post(new Runnable() {
+                                                        @Override
+                                                        public void run() {
+                                                            title.setText(data_title);
+                                                            my_string.setText("        " + data_text);
+                                                        }
+                                                    });
+                                                } else {
+                                                    progressDialog.dismiss();
+                                                    Toast.makeText(nextActivity.this, "章节异常", Toast.LENGTH_LONG).show();
+                                                }
+                                            } catch (IOException e) {
+                                                e.printStackTrace();
+                                                Log.e("nextActivity", "");
+                                            }
+                                        }
+                                    }).start();
+                                    aler.dismiss();
+                                } else {
+                                    Toast.makeText(nextActivity.this, "没有上一章了", Toast.LENGTH_LONG).show();
+                                }
+                            } catch (Exception e) {
+                                Log.e("click error", e.getMessage());
+                            }
+                        }
+
+                    });
+                    down.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            msg.setText("搜索中,请稍等");
+                            progressDialog.show();
+                            if (paixu.equals("降序")) {
+                                nunber1 = nunber1 - 1;
+                            } else {
+                                nunber1 = nunber1 + 1;
+                            }
+                            if (nunber1 < 0) {
+                                progressDialog.dismiss();
+                                nunber1 = nunber1 + 1;
+                                Toast.makeText(nextActivity.this, "没有下一章了", Toast.LENGTH_LONG).show();
+                                return;
+                            }
+                            chapter_url = list_chapter_url.get(nunber1);
+                            new Thread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    try {
+                                        Document doc = Jsoup.connect(chapter_url).get();
+                                        Elements btEl = doc.select("#content");
+                                        final String data_title = doc.select("h1").text();
+                                        btEl.select("br").next().append("\\n");
+                                        String bt1 = btEl.text();
+                                        final String data_text = bt1.replace("\\n", "\n       ");
+                                        if (!data_text.equals("")) {
+                                            progressDialog.dismiss();
+                                            Handler mainHandler = new Handler(Looper.getMainLooper());
+                                            mainHandler.post(new Runnable() {
+                                                @Override
+                                                public void run() {
+                                                    title.setText(data_title);
+                                                    my_string.setText("        " + data_text);
+                                                }
+                                            });
+                                        } else {
+                                            progressDialog.dismiss();
+                                            Toast.makeText(nextActivity.this, "章节异常", Toast.LENGTH_LONG).show();
+                                        }
+                                    } catch (IOException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                            }).start();
+                            aler.dismiss();
+                        }
+                    });
+                    Window window = aler.getWindow();
+                    window.setBackgroundDrawable(null); // 重设background
+                    WindowManager.LayoutParams wlp = window.getAttributes();
+                    wlp.gravity = Gravity.BOTTOM;
+                    WindowManager wm = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
+                    Display display = wm.getDefaultDisplay();
+                    wlp.width = display.getWidth();
+                    aler.getWindow().setBackgroundDrawable(null);
+                    window.setAttributes(wlp);
                 }
             }
         });
     }
+
 };
+
